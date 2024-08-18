f798f3e132b2b5b81ee475c7c1f0bb8490895154
Aman Kumar <amanupadhayay2906@gmail.com>
2024-08-18 21:19:24 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,20 +1,22 @@
 import React from "react"
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from '../redux/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 // import OAuth from '../components/OAuth';
 
 
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const { loading, error } = useSelector((state) => state.user);
+  // loading and error are destructured from the user slice of the redux store
+  const { loading, error } = useSelector((state) => state.user); // coming from global state
   const navigate = useNavigate();
+  // dispatch is used to dispatch actions to the redux store
   const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
@@ -25,7 +27,8 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      dispatch(signInStart());
+      // using redux thunk to dispatch the signInStart action to set loading to true before making the request to the server 
+      dispatch(signInStart()); 
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -35,6 +38,7 @@ export default function SignIn() {
       });
       const data = await res.json();
       console.log(data);
+      // if the success is false, we dispatch the signInFailure action to set the error to the message from the server using redux thunk
       if (data.success === false) {
         dispatch(signInFailure(data.message));
         return;
