52a78afa534b4c1641b12634d46bcc0221574d04
Aman Kumar <amanupadhayay2906@gmail.com>
2024-08-18 15:22:16 +0530

    complete signup page functionallity

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,38 +4,64 @@ import { Link, useNavigate } from 'react-router-dom';
 // import OAuth from '../components/OAuth';
 
 export default function SignUp() {
+  // formData object will be changed using the setFormData function which is initially an empty object
   const [formData, setFormData] = useState({});
+  // error state to display the error message if there is an error
   const [error, setError] = useState(null);
+  // loading state to display the loading message if the data is loading
   const [loading, setLoading] = useState(false);
+  // navigate function to navigate to the sign in page
   const navigate = useNavigate();
+
+  // function to handle the change of the input - take an event 
   const handleChange = (e) => {
+    // set the form data to the current form data and update the id of the target to the value of the target
     setFormData({
+      // spread operator to get the current form data
       ...formData,
+      // add the new change to the form data
       [e.target.id]: e.target.value,
     });
   };
+  // console.log(formData);
+
+
+  // function to handle the submit of the form - take an event
   const handleSubmit = async (e) => {
+    // prevent the default action of the form - i.e prevent refreshing the page when the form is submitted
     e.preventDefault();
     try {
       setLoading(true);
+      // fetch method to request out api route and to get the login data
+      // each time you see the /api add the localhost 3000 from the proxy sever defined in vite.config.in
       const res = await fetch('/api/auth/signup', {
+        // POST method to send the data to the server
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
+        // changes to string for security purpose
+        // send from the body of the browser and send these data in string format
         body: JSON.stringify(formData),
       });
+      // now again changing the response we got in json 
       const data = await res.json();
       console.log(data);
+
+      // if the data is not successful then set the loading to false and set the error to the message
       if (data.success === false) {
         setLoading(false);
         setError(data.message);
         return;
       }
+      // if data is successful then set the loading to false and set the error to null and navigate to the sign in page
       setLoading(false);
       setError(null);
       navigate('/sign-in');
-    } catch (error) {
+    } 
+    // everything under try and catch to run smoothly and catch the error if there is any
+    // if submitted without any data then it will show the error message
+    catch (error) {
       setLoading(false);
       setError(error.message);
     }
@@ -51,6 +77,7 @@ export default function SignUp() {
           className='border p-3 rounded-lg'
           // adding id to let us know if it get changed later on 
           id='username'
+          // onchange event listener - when the input changes we want to call the handleChange function
           onChange={handleChange}
         />
         <input // email
@@ -68,7 +95,7 @@ export default function SignUp() {
           onChange={handleChange}
         />
 
-        <button
+        <button // submit button - loading is true then show loading else show sign up
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70'
         >
