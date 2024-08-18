961b2c667c62258aaec12631eaf2fd82930a3d1b
Aman Kumar <amanupadhayay2906@gmail.com>
2024-08-19 01:28:04 +0530

    update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,36 +1,54 @@
 import React from "react";
-import { FaSearch } from 'react-icons/fa'; // font awesome search icon
+import { FaSearch } from 'react-icons/fa';
 import { Link, useNavigate } from 'react-router-dom';
-//import { useSelector } from 'react-redux';
-//import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  // get the current user from the redux store
+  const { currentUser } = useSelector((state) => state.user); // saved the name as user in userSlice
+  // initialize the searchTerm state and setSearchTerm function
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
-    <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-          <span className="text-slate-500">Upadhayay</span>
-          <span className="text-slate-700">Estate</span>
-        </h1>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Sahand</span>
+            <span className='text-slate-700'>Estate</span>
+          </h1>
         </Link>
-        {/* Search form to search anything in the site*/}
         <form
-            className='bg-slate-100 p-3 rounded-lg flex items-center'
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
         >
-            <input
-                type='text'
-                placeholder="Search..."
-                className="bg-transparent focus:outline-none w-24 sm:w-64"
-            />
-
-            <button>
-                <FaSearch className='text-slate-600' />
-            </button>
-
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
         </form>
-
-        {/* // Menu items for navigation */}
         <ul className='flex gap-4'>
           <Link to='/'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>
@@ -42,14 +60,19 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to='/sign-in'>
-            
+          <Link to='/profile'>
+            {/* if the current user exists display the user's avatar else display the sign in link */}
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
               <li className=' text-slate-700 hover:underline'> Sign in</li>
-            
+            )}
           </Link>
         </ul>
-
-
       </div>
     </header>
   );
