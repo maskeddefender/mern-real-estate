e7c796266f0531bfc5696d3e3bc39af3617de540
Aman Kumar <amanupadhayay2906@gmail.com>
2024-08-18 12:43:25 +0530

    create a middleware and a function to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,10 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs'; // to hash the password before saving it to the database so that it cannot be read by anyone
+import { errorHandler } from '../utils/error.js';
 
 
 // get the information from the browser when the user is signing up, this is coming from the body of the request
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
 
     // to save the content from the body to the variables 
     // this gives us the flexibility to change the details of the variables in the future
@@ -18,6 +19,7 @@ export const signup = async (req, res) => {
         await newUser.save() // as the .save takes some time to save the information to the database, we use the async and await to wait for the information to be saved before we send the response
         res.status(201).json('User created successfully');
     } catch (error) {
-        res.status(500).json(error.message);
+        // if there is an error, we send the error to the next middleware to handle the error 
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -34,3 +34,23 @@ app.listen(3000, () => {
 // this is in the routes folder - user.route.js
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+
+
+
+// api middleware to handle errors
+// error - is the error coming form the input of the middleware
+// req - is the request data from the browser
+// res - response from the server to the client side
+// next - is the next middleware to be executed
+app.use((err, req, res, next) => {
+    // statusCode - is the status code of the error else it will be 500 
+    const statusCode = err.statusCode || 500;
+    // message - is the message of the error else it will be 'Internal Server Error'
+    const message = err.message || 'Internal Server Error';
+    // return the status code and the message of the error
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+});
