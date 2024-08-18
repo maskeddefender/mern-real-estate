c455e09cd458ca94c391a031d943cb903bfb34bf
Aman Kumar <amanupadhayay2906@gmail.com>
2024-08-18 21:47:14 +0530

    redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { store } from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 import { Provider } from 'react-redux';
+// PersistGate krta ye hai ki jab tak data local storage me save nhi hota tab tak loading screen show krta hai - jab data save ho jaata hai tab tak loading screen show hota hai
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+  
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
